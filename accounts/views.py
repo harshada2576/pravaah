8e41ade1de9fe0077de9e5b694fa8d37f0966fbe
Hardik Patil <hardikpatil2745@gmail.com>
2026-05-28 15:01:22 +0530

    dashboard updated

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
-from .models import User
-from .models import Role
-from .models import AuditLog
-from django.core.paginator import Paginator
-from .models import User
-from .models import Role
-from django.contrib.auth.decorators import login_required
-from .forms import ProfileEditForm
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
 from django.core.paginator import Paginator
+from django.db import models
+from .models import User
+from .models import Role
+from .models import AuditLog
+from .forms import ProfileEditForm
 def home(request):
     return render(
         request,
@@ -19,44 +17,77 @@ def home(request):
     )
 @login_required
 def profile(request):
+
     context = {
+
         'user_data': request.user
+
     }
+
     return render(
+
         request,
+
         'accounts/profile.html',
+
         context
+
     )
 @login_required
 def profile_edit(request):
+
     user = request.user
+
     if request.method == 'POST':
+
         form = ProfileEditForm(
+
             request.POST,
+
             instance=user
+
         )
+
         if form.is_valid():
+
             form.save()
+
             # AUDIT LOG
+
             AuditLog.objects.create(
+
                 user=user,
+
                 action='Profile Updated',
+
                 ip_address=request.META.get(
+
                     'REMOTE_ADDR'
+
                 )
+
             )
+
             messages.success(
+
                 request,
+
                 'Profile updated successfully.'
 
             )
 
             return redirect(
+
                 '/profile/'
+
             )
+
     else:
+
         form = ProfileEditForm(
+
             instance=user
+
         )
 
     context = {
@@ -76,31 +107,69 @@ def profile_edit(request):
     )
 
 
+# ==========================================
+# CHANGE PASSWORD
+# ==========================================
+
 @login_required
 def change_password(request):
+
     if request.method == 'POST':
+
         form = PasswordChangeForm(
+
             request.user,
+
             request.POST
+
         )
+
         if form.is_valid():
+
             user = form.save()
+
             update_session_auth_hash(
+
                 request,
+
                 user
+
+            )
+
+            # AUDIT LOG
+
+            AuditLog.objects.create(
+
+                user=request.user,
+
+                action='Password Changed',
+
+                ip_address=request.META.get(
+
+                    'REMOTE_ADDR'
+
+                )
             )
             messages.success(
+
                 request,
+
                 'Password changed successfully.'
+
             )
+
             return redirect(
+
                 '/profile/'
+
             )
 
     else:
 
         form = PasswordChangeForm(
+
             request.user
+
         )
 
     context = {
@@ -119,49 +188,58 @@ def change_password(request):
 
     )
 
+
+# ==========================================
+# AUDIT LOGS
+# ==========================================
+
+@login_required
 def audit_logs(request):
 
+    search = request.GET.get('search')
+
     logs = AuditLog.objects.all().order_by(
+
         '-created_at'
-    )
 
-    # SEARCH
-
-    search = request.GET.get(
-        'search'
     )
 
     if search:
 
         logs = logs.filter(
-            user__username__icontains=search
+
+            models.Q(
+
+                action__icontains=search
+
+            ) |
+
+            models.Q(
+
+                user__username__icontains=search
+
+            )
+
         )
-
-    # ACTION FILTER
-
-    action = request.GET.get(
-        'action'
-    )
-
-    if action:
-
-        logs = logs.filter(
-            action=action
-        )
-
-    # PAGINATION
 
     paginator = Paginator(
+
         logs,
+
         10
+
     )
 
     page_number = request.GET.get(
+
         'page'
+
     )
 
     page_obj = paginator.get_page(
+
         page_number
+
     )
 
     context = {
@@ -170,9 +248,7 @@ def audit_logs(request):
 
         'page_obj': page_obj,
 
-        'search': search,
-
-        'selected_action': action
+        'search': search
 
     }
 
@@ -185,136 +261,52 @@ def audit_logs(request):
         context
 
     )
+
+
+# ==========================================
+# REPORTS
+# ==========================================
+
 @login_required
-def activity_dashboard(request):
-
-    total_users = User.objects.count()
-
-    total_logs = AuditLog.objects.count()
-
-    total_roles = Role.objects.count()
-
-    login_count = AuditLog.objects.filter(
-        action='Login'
-    ).count()
-
-    recent_logs = AuditLog.objects.all().order_by(
-        '-created_at'
-    )[:5]
-
-    # -------------------------
-    # ROLE DISTRIBUTION
-    # -------------------------
-
-    admin_count = User.objects.filter(
-        role__role_name='Admin'
-    ).count()
-
-    manager_count = User.objects.filter(
-        role__role_name='Manager'
-    ).count()
-
-    qa_count = User.objects.filter(
-        role__role_name='QA'
-    ).count()
-
-    # -------------------------
-    # STATUS COUNTS
-    # -------------------------
-
-    active_users = User.objects.filter(
-        status='Active'
-    ).count()
-
-    inactive_users = User.objects.filter(
-        status='Inactive'
-    ).count()
-
-    # -------------------------
-    # AUDIT ACTIONS
-    # -------------------------
-
-    login_actions = AuditLog.objects.filter(
-        action='Login'
-    ).count()
-
-    logout_actions = AuditLog.objects.filter(
-        action='Logout'
-    ).count()
-
-    update_actions = AuditLog.objects.filter(
-        action='Profile Updated'
-    ).count()
-
-    context = {
-
-        'total_users': total_users,
-
-        'total_logs': total_logs,
-
-        'total_roles': total_roles,
-
-        'login_count': login_count,
-
-        'recent_logs': recent_logs,
-
-        # ROLE DATA
-
-        'admin_count': admin_count,
-
-        'manager_count': manager_count,
-
-        'qa_count': qa_count,
-
-        # STATUS DATA
-
-        'active_users': active_users,
-
-        'inactive_users': inactive_users,
-
-        # AUDIT DATA
-
-        'login_actions': login_actions,
-
-        'logout_actions': logout_actions,
-
-        'update_actions': update_actions
-
-    }
-
-    return render(
-
-        request,
-
-        'accounts/activity_dashboard.html',
-
-        context
-
-    )
-
 def reports(request):
 
     return render(
+
         request,
+
         'accounts/reports.html'
+
     )
 
 
+# ==========================================
+# REPORT RESULT
+# ==========================================
+
+@login_required
 def report_result(request):
 
     report_type = request.GET.get(
+
         'report_type'
+
     )
 
     role = request.GET.get(
+
         'role'
+
+    )
+
+    search = request.GET.get(
+
+        'search'
+
     )
 
     data = []
 
-    # -------------------------
     # USERS REPORT
-    # -------------------------
 
     if report_type == 'users':
 
@@ -323,14 +315,22 @@ def report_result(request):
         if role:
 
             users = users.filter(
+
                 role__role_name=role
+
+            )
+
+        if search:
+
+            users = users.filter(
+
+                username__icontains=search
+
             )
 
         data = users
 
-    # -------------------------
-    # ROLE REPORT
-    # -------------------------
+    # ROLES REPORT
 
     elif report_type == 'roles':
 
@@ -339,20 +339,38 @@ def report_result(request):
         if role:
 
             users = users.filter(
+
                 role__role_name=role
+
+            )
+
+        if search:
+
+            users = users.filter(
+
+                username__icontains=search
+
             )
 
         data = users
 
-    # -------------------------
     # LOGIN REPORT
-    # -------------------------
 
     elif report_type == 'logins':
 
         logs = AuditLog.objects.filter(
+
             action='Login'
+
         )
+
+        if search:
+
+            logs = logs.filter(
+
+                user__username__icontains=search
+
+            )
 
         data = logs
 
@@ -360,7 +378,9 @@ def report_result(request):
 
         'report_type': report_type,
 
-        'data': data
+        'data': data,
+
+        'search': search
 
     }
 
@@ -374,38 +394,56 @@ def report_result(request):
 
     )
 
+
+# ==========================================
+# USERS LIST
+# ==========================================
+
 @login_required
 def users_list(request):
 
     users = User.objects.all().order_by(
+
         '-created_at'
+
     )
 
     # SEARCH
 
     search = request.GET.get(
+
         'search'
+
     )
 
     if search:
 
         users = users.filter(
+
             username__icontains=search
+
         )
 
     # PAGINATION
 
     paginator = Paginator(
+
         users,
+
         10
+
     )
 
     page_number = request.GET.get(
+
         'page'
+
     )
 
     page_obj = paginator.get_page(
+
         page_number
+
     )
 
     context = {
@@ -423,6 +461,164 @@ def users_list(request):
         request,
 
         'accounts/users_list.html',
+
+        context
+
+    )
+
+
+# ==========================================
+# ACTIVITY DASHBOARD
+# ==========================================
+
+@login_required
+def activity_dashboard(request):
+
+    total_users = User.objects.count()
+
+    total_logs = AuditLog.objects.count()
+
+    total_roles = Role.objects.count()
+
+    login_count = AuditLog.objects.filter(
+
+        action='Login'
+
+    ).count()
+
+    recent_logs = AuditLog.objects.all().order_by(
+
+        '-created_at'
+
+    )[:5]
+
+    # ROLE DISTRIBUTION
+
+    admin_count = User.objects.filter(
+
+        role__role_name='Admin'
+
+    ).count()
+
+    manager_count = User.objects.filter(
+
+        role__role_name='Manager'
+
+    ).count()
+
+    qa_count = User.objects.filter(
+
+        role__role_name='QA'
+
+    ).count()
+
+    # USER STATUS
+
+    active_users = User.objects.filter(
+
+        status='Active'
+
+    ).count()
+
+    inactive_users = User.objects.filter(
+
+        status='Inactive'
+
+    ).count()
+
+    # AUDIT ACTIONS
+
+    login_actions = AuditLog.objects.filter(
+
+        action='Login'
+
+    ).count()
+
+    logout_actions = AuditLog.objects.filter(
+
+        action='Logout'
+
+    ).count()
+
+    update_actions = AuditLog.objects.filter(
+
+        action='Profile Updated'
+
+    ).count()
+
+    # EXTRA ANALYTICS
+
+    active_users_count = User.objects.filter(
+
+        status='Active'
+
+    ).count()
+
+    recent_registrations = User.objects.order_by(
+
+        '-created_at'
+
+    )[:5]
+
+    failed_logins = AuditLog.objects.filter(
+
+        action='Failed Login'
+
+    ).count()
+
+    top_role = Role.objects.annotate(
+
+        total_users=models.Count('user')
+
+    ).order_by(
+
+        '-total_users'
+
+    ).first()
+
+    context = {
+
+        'total_users': total_users,
+
+        'total_logs': total_logs,
+
+        'total_roles': total_roles,
+
+        'login_count': login_count,
+
+        'recent_logs': recent_logs,
+
+        'admin_count': admin_count,
+
+        'manager_count': manager_count,
+
+        'qa_count': qa_count,
+
+        'active_users': active_users,
+
+        'inactive_users': inactive_users,
+
+        'login_actions': login_actions,
+
+        'logout_actions': logout_actions,
+
+        'update_actions': update_actions,
+
+        'active_users_count': active_users_count,
+
+        'recent_registrations': recent_registrations,
+
+        'failed_logins': failed_logins,
+
+        'top_role': top_role
+
+    }
+
+    return render(
+
+        request,
+
+        'accounts/activity_dashboard.html',
 
         context
 
